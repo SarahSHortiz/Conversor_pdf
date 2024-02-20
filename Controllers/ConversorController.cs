5176a73db5586f557de61ce9850e13b6f587fa71
Sarah <112779900+SarahSHortiz@users.noreply.github.com>
2024-02-19 22:40:50 -0300

    Add files via upload

---
+++ Controllers/ConversorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorDePDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorController : ControllerBase
+    {
+    }
+}
---
+++ Controllers/Leitor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConversorDePDF.Controllers
+{
+    public class Leitor : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
